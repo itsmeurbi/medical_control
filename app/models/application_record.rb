429ad1e360e090693f624777644c8dc5bdc55517
ballo1988 <rounderjards@gmail.com>
2024-05-07 22:42:27 -0600

    Added export option to save Patients records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
+require 'csv'
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.to_csv
+    attributes = column_names
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.find_each do |patient|
+        csv << attributes.map { |attr| patient.send(attr) }
+      end
+    end
+  end
 end
