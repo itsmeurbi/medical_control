7fcc5fefaf34cca97a5569cf2371e3922513e7b1
Diego Zaizar <diegozaizar26@gmail.com>
2024-06-04 23:04:10 -0600

    Create controller edit action and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :patients do
-    resources :consultations, only: %i[create index]
+    resources :consultations, only: %i[create index edit update]
   end
 
   root 'patients#index'
