c1d58db12ce87f0ff5009a8347d1af67aa4a9a8b
Miguel Urbina <itismeurbi@gmail.com>
2025-05-25 09:32:20 -0600

    Remove phone number validatinos

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -15,9 +15,6 @@ class Patient < ApplicationRecord
   enum blood_type: { a: 0, b: 1, ab: 2, o: 3 }
   enum :rh_factor, { negativo: 0, positivo: 1 }, prefix: :rh
   validates :name, :registered_at, :gender, presence: true
-  validates :phone_number, :cellphone_number, length: { is: 10 }, allow_blank: true
-  validates :phone_number, :cellphone_number, format: { with: /\A\d+\Z/,
-                                                        message: 'only allows numbers' }, allow_blank: true
 
   has_many :treatments, class_name: 'Consultation', inverse_of: :patient, dependent: :destroy
 
